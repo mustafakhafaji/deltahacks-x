45d114fbd03eceb4a16e66ba7b180fe780ef1ff0
kcarnrite <kfcarnrite@gmail.com>
2024-01-13 15:48:13 -0500

    Added basic function for calculating carbon footprint

---
+++ src/emissionCalculator.js
@@ -0,0 +1,16 @@
+// returns in kilograms
+function calculateCarbonFootprint(transportType, distance) {
+    switch(transportType) {
+        case gasCar:
+            return (distance * 170)/1000;
+        case electricCar:
+            return (distance * 47)/1000;
+        case bus:
+            return (distance * 79)/1000;
+        case walk:
+            return (distance * 39)/1000;
+        case cycle:
+            return (distance* 39)/1000;
+    }
+
+}

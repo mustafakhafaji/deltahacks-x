dd527168695728564fff6604e64db4f5c86bee08
Dominic Chen <dominic.chen630@gmail.com>
2024-01-14 01:46:53 -0500

    MARKERS

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,9 @@
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
 // Stupid shit only runs Apple Maps on MacOS.
-function Map({ coordinates }) {
+function Map({ coordinates, startend }) {
 
   return (
     <MapView
@@ -14,7 +14,10 @@ function Map({ coordinates }) {
         longitude: -coordinates[1],
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
-      }}/>
+      }}>
+      {startend[0] && <Marker coordinate={{ latitude: startend[0][0], longitude: -startend[0][1] }} />}
+      {startend[1] && <Marker coordinate={{ latitude: startend[1][0], longitude: -startend[1][1] }} />}
+      </MapView>
   );
 };
 

5407ae8defa22bfc98d22ad1d1c7bbe21ea3a1d9
Mustafa Khafaji <143281566+mustafakhafaji@users.noreply.github.com>
2024-01-13 20:02:59 -0500

    Caffeine got me going crazy

--- App.js
+++ App.js
@@ -4,11 +4,16 @@ import SearchBar from './src/components/SearchBar';
 import Map from './src/components/Map'; 
 import Menu from './src/components/Menu';
 
+const init_location = [43.6532, 79.3832]
+
 export default function App() {
-  const [current_location, setCurrentLocation] = useState([43.6532, 79.3832]);
+  const [current_location, setCurrentLocation] = useState(init_location);
+  const [start_location, setStartLocation] = useState(init_location);
+  const [end_location, setEndLocation] = useState([0, 0]);
+  
 
   // Parse location data. 
-  const handleOnSearchSubmit = (data, details) => {
+  const handleLocationSubmit = (data, details, location_type) => {
     const latitude = details.geometry.location.lat;
     const longitude = details.geometry.location.lng;
     setCurrentLocation([latitude, -longitude]);
@@ -19,11 +24,15 @@ export default function App() {
     // Send distance data to menu.
   }
 
+  const handleDirectionsSubmit = () => {
+    // just send start and destination location to map to process.
+  }
+
   // Current location appears to be wrong?
   return (
     <View>
       <Map coordinates={current_location} key={current_location[0]+current_location[1]}/>
-      <SearchBar onSearchSubmit={handleOnSearchSubmit}/>
+      <SearchBar locationSubmit={handleLocationSubmit} directionsSubmit={handleDirectionsSubmit}/>
       
       {current_location.length !== 0 && <Menu />}
     </View>

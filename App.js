ec94c08793e9527e297c64b0e80161720f7e98c0
Stephen Tao <81048100+stephzilla20@users.noreply.github.com>
2024-01-14 09:15:44 -0500

    solve merge stuff

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import SearchBar from './src/components/SearchBar';
-import Map from './src/components/Map'; 
-import Menu from './src/components/InfoMenu';
+import { LogBox } from 'react-native';
+import Home from './src/screens/Home';
+import Log from './src/screens/Log';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NavBar from './src/components/NavBar';
+const Stack = createNativeStackNavigator();
 
+function Nav(){
+  return (
+    <NavBar/>
+
+  )
+}
 
 export default function App() {
   return(
-    <NavBar/>
+
+    //create 2 routes to Home and Log, 
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/> 
+          <Stack.Screen name="Log" component={Nav} options={{ headerShown: false }}/> 
+
+      </Stack.Navigator>
+    </NavigationContainer>
+
  
   );
 }
+

c37e36c1deb71c845ef5f05edf15f98580aea8d2
kcarnrite <kfcarnrite@gmail.com>
2024-01-14 10:36:10 -0500

    graphtitle

---
+++ src/components/GraphTitle.js
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+
+function GraphTitle({titleText}) {
+    return (
+        <Text style={{alignSelf: 'center'}}>{titleText}</Text>
+    )
+
+}
+
+export default GraphTitle;
